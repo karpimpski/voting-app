eca9c72488c6a42fb48cbb797862e9d51fea646c
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-10 17:23:13 -0500

    can now add more options

--- client/src/NewPoll/index.js
+++ client/src/NewPoll/index.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Client from '../Client';
 import {Link} from 'react-router';
 import {proxy} from '../../package.json';
 
 class NewPoll extends Component {
+	constructor(props){
+		super(props);
+		this.state = {num: 3}
+	}
+
+	addOption(e){
+		e.preventDefault();
+		this.setState({num: this.state.num + 1})
+	}
+
 	render() {
+		var inputs = [];
+		for(var i = 0; i < this.state.num; i++){
+			inputs.push(<div key={i}><input name='option'/><br/></div>)
+		}
 		return (
 			<form action={proxy + 'api/newpoll'}>
 				<input name='name' placeholder='name'/>
-				<input name='option'/>
-				<input name='option'/>
-				<input name='option'/>
+				{inputs}
+				<button onClick={this.addOption.bind(this)}>Add</button>
 				<input type='submit'/>
 			</form>
 		)
