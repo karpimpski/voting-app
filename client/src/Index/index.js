ee9e3c874d4506fe9ff8c705b35c70da1f6e8ab2
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-31 00:53:09 -0500

    index only shows 4 polls, uses pages

--- client/src/Index/index.js
+++ client/src/Index/index.js
@@ -6,12 +6,13 @@ import Header from '../Header';
 class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = {polls: {}, user: null};
+    this.state = {polls: [], user: null, currentPolls: [], pollIndex: 0};
   }
 
   componentDidMount(){
     Client.get('/api/polls', (res) => {
-      this.setState({polls: res});
+      this.setState({polls: res, currentPolls: res.slice(this.state.pollIndex, this.state.pollIndex + 4)});
+      console.log(this.state.currentPolls);
     });
     Client.get('/api/currentuser', (res) => {
       this.setState({user: res.res})
@@ -25,21 +26,43 @@ class Index extends Component {
     }
   }
 
+  nextPage(){
+    if(this.state.pollIndex < this.state.polls.length - 4){
+      this.setState({
+        currentPolls: this.state.polls.slice(this.state.pollIndex + 4, this.state.pollIndex + 8),
+        pollIndex: this.state.pollIndex + 4
+      });
+    }
+  }
+
+  previousPage(){
+    if(this.state.pollIndex > 0){
+      this.setState({
+        currentPolls: this.state.polls.slice(this.state.pollIndex - 4, this.state.pollIndex),
+        pollIndex: this.state.pollIndex - 4
+      });
+    }
+  }
+
   render(){
     return (
       <div id='polls'>
-      <Header />
-      {Object.keys(this.state.polls).map((key, i) => {
-        var poll = this.state.polls[key]
-        return (
-          <div className='center row' key={i}>
-            <Link to={`/poll/${encodeURIComponent(poll.name)}`}><div className='tall wide button'>{poll.name}</div></Link>
+        <Header />
+          <div id='poll-list'>
+          {Object.keys(this.state.currentPolls).map((key, i) => {
+            var poll = this.state.currentPolls[key]
+            return (
+              <div className='center row' key={i}>
+                <Link to={`/poll/${encodeURIComponent(poll.name)}`}><div className='tall wide button'>{poll.name}</div></Link>
+              </div>
+            )
+          })}
+          <div className='center row'>
+            {this.state.pollIndex > 0 ? <div className='tall thin button' onClick={this.previousPage.bind(this)}>Previous</div> : null}
+            <Link to={`/newpoll`} onClick={this.checkVote.bind(this)}><div className='tall thin button'>New Poll</div></Link>
+            {this.state.pollIndex < this.state.polls.length - 4 ? <div className='tall thin button' onClick={this.nextPage.bind(this)}>Next</div> : null}
           </div>
-        )
-      })}
-      <div className='center row'>
-        <Link to={`/newpoll`} onClick={this.checkVote.bind(this)}><div className='tall wide button'>New Poll</div></Link>
-      </div>
+        </div>
       </div>
     );
   }
